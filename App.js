4e246f1ce8a3cf139dcedeb85818e2d8b1946fbf
rashedul.islam <ririyad.291@gmail.com>
2018-04-22 13:44:14 +0600

    flexed dimension

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
 
-export default class FixedDimensionBasics extends Component {
+export default class FlexDimensionBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{ width: 50, height: 50, backgroundColor : 'powderblue' }}/>
-        <View style={{ width: 100, height: 100, backgroundColor : 'skyblue' }}/>
-        <View style={{ width: 150, height: 150, backgroundColor : 'steelblue' }}/>
+      <View style = {{flex: 1}} >
+        <View style={{flex:1, backgroundColor : 'powderblue' }}/>
+        <View style={{flex: 2, backgroundColor : 'skyblue' }}/>
+        <View style={{flex:3, backgroundColor : 'steelblue' }}/>
         </View>
     );
   }
